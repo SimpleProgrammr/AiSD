a27654a5e38687a3d0cfb772240f47e64e89fbca
Programmr <michal.pelon@proton.me>
2026-03-03 19:43:18 +0100

    Bugfix

--- Integrals-Project/integrals.cpp
+++ Integrals-Project/integrals.cpp
@@ -1,12 +1,18 @@
 #include <complex>
 #include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
 #include <random>
 #include <sys/time.h>
 
+#define timersub(tvp,uvp,vvp) do {(vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec; (vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec; if ((vvp)->tv_usec < 0) {(vvp)->tv_sec--;(vvp)->tv_usec += 1000000;}} while (0)
+
 using namespace std;
 
-double easy_fun(double x) {
-    return exp(-x);
+//Tu zapisz swoją funkcję :)
+double FUNCTION_TO_TEST(double x) {
+    return 1-x;
 }
 
 double left_rectangles_integral(double a, double b, long double parts, const function<double(double x)> &func) {
@@ -85,7 +91,7 @@ double monte_carlo_integral(double a, double b, double bottom, double top, long 
         }
     }
 
-    return (abs(top-bottom)*abs(a-b))*hits/static_cast<long double>(tries);
+    return abs(top-bottom)*abs(a-b)*hits/static_cast<long double>(tries);
 }
 
 void raport_normal_integral(long N,
@@ -101,7 +107,7 @@ void raport_normal_integral(long N,
 
     gettimeofday(&startTV, nullptr);
 
-    double surface = func(a, b, N, easy_fun);
+    double surface = func(a, b, N, FUNCTION_TO_TEST);
 
     gettimeofday(&endTV, nullptr);
 
@@ -127,7 +133,7 @@ void raport_probabilistic_integral(long N,
 
     gettimeofday(&startTV, nullptr);
 
-    double surface = func(a, b,bottom,top, N, easy_fun);
+    double surface = func(a, b,bottom,top, N, FUNCTION_TO_TEST);
 
     gettimeofday(&endTV, nullptr);
 
@@ -137,33 +143,66 @@ void raport_probabilistic_integral(long N,
 }
 
 int main() {
-    ofstream LEFT("LEFT.txt"), RIGHT("RIGHT.txt"), MID("MID.txt"), TRAP ("TRAP.txt"), MONTE("MONTE.txt");
+    constexpr long max_N = 10000;
+    const double a = 0;
+    const double b = 1;
 
-            double a=0;
-            double b=1;
-#pragma omp parallel for nowait
-    for (long N = 1; N < 100000; N+=1) {
+    // Otwieramy pliki poza sekcją równoległą
+    ofstream LEFT("LEFT.txt");
+    ofstream RIGHT("RIGHT.txt");
+    ofstream MID("MID.txt");
+    ofstream TRAP("TRAP.txt");
+    ofstream MONTE("MONTE.txt");
 
-#pragma omp sections
+    if (!LEFT || !RIGHT || !MID || !TRAP || !MONTE) {
+        cerr << "Błąd otwierania plików!" << std::endl;
+        return 1;
+    }
+
+    LEFT << "N\tCalculated Value\tTime"<<endl;
+    RIGHT << "N\tCalculated Value\tTime"<<endl;
+    MID << "N\tCalculated Value\tTime"<<endl;
+    TRAP << "N\tCalculated Value\tTime"<<endl;
+    MONTE << "N\tCalculated Value\tTime"<<endl;
+
+    timeval diff{}, startTV{}, endTV{};
+    gettimeofday(&startTV, nullptr);
+
+    for (long N = 1; N < max_N; N += 1) {
+
+#pragma omp parallel
         {
+#pragma omp sections
+            {
 #pragma omp section
-            raport_normal_integral(N, a, b, left_rectangles_integral, (LEFT));
+                raport_normal_integral(N, a, b, left_rectangles_integral, LEFT);
+
 #pragma omp section
-            raport_normal_integral(N, a, b, right_rectangles_integral, (RIGHT));
+                raport_normal_integral(N, a, b, right_rectangles_integral, RIGHT);
+
 #pragma omp section
-            raport_normal_integral(N, a, b, mid_rectangles_integral, (MID));
+                raport_normal_integral(N, a, b, mid_rectangles_integral, MID);
+
 #pragma omp section
-            raport_normal_integral(N, a, b, trapezoids_integral, (TRAP));
+                raport_normal_integral(N, a, b, trapezoids_integral, TRAP);
+
 #pragma omp section
-            raport_probabilistic_integral(N,a,b,0,2,monte_carlo_integral, (MONTE));
+                raport_probabilistic_integral(N, a, b, 0, 2, monte_carlo_integral, MONTE);
+            }
         }
     }
-#pragma omp barrier
+    gettimeofday(&endTV, nullptr);
+    timersub(&endTV, &startTV, &diff);
 
+    cout << "Calculations time: " << diff.tv_usec/1000.0L+ static_cast<long double>(diff.tv_sec) * 1000L << "ms\n\n" << endl;
+
+    // Zamknięcie plików
     LEFT.close();
     RIGHT.close();
     MID.close();
     TRAP.close();
     MONTE.close();
+
+    return 0;
 }
 
