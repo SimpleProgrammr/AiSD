a439b9600d501e05d97d3b06b5dd25219ff9c9d2
Programmr <michal.pelon@proton.me>
2026-04-03 18:57:08 +0200

    Minor bug fix

--- Sorting-Project/quick_sort.h
+++ Sorting-Project/quick_sort.h
@@ -5,19 +5,26 @@
 #pragma once
 #include <bits/move.h>
 
-inline int * quick_sort(int * data, unsigned int length) {
-    int pivot = data[length - 1];
+int pivot(int* data, int low, int high) {
+    int piv = data[high];
 
-    int i = 0, j = 0;
-    while (j <= length - 2) {
-        if (data[j] > pivot) {
-            std::swap(data[++i], data[j++]);
-        }
-        else {
-            j++;
+    int i = low-1;
+    for (int j = low; j <= high-1; j++) {
+        if (data[j] < piv) {
+            std::swap(data[++i], data[j]);
         }
     }
+    std::swap(data[++i], data[high]);
+    return i;
+}
 
+inline int * quick_sort(int * data, int low, int high) {
+    if (low < high) {
+        int pi = pivot(data, low, high);
+
+        quick_sort(data, low, pi - 1);
+        quick_sort(data, pi + 1, high);
+
+    }
     return data;
-
 }
