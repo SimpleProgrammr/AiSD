092710f5cb88b7a67c0d9cd2803e9b11b889ad72
Programmr <michal.pelon@proton.me>
2026-04-04 18:52:30 +0200

    Miracle sort init

---
+++ Sorting-Project/miracle_sort.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Michał on 04.04.2026.
+//
+#pragma once
+
+#include <list>
+std::list<int> miracle_sort(std::list<int> data) {
+    bool isSorted;
+    do {
+        isSorted = true;
+        int prev = data.front();
+        for (auto i = data.begin(); i != data.end(); ++i) {
+            if (prev > *i) {
+                isSorted = false;
+                break;
+            }
+            prev = *i;
+            ++i;
+        }
+    }while (!isSorted);
+
+    return data;
+}
