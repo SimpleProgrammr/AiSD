7a11a85420a9b03a4bd44537dac141b01a4f738b
Programmr <michal.pelon@proton.me>
2026-04-04 18:53:34 +0200

    Stalin sort init

---
+++ Sorting-Project/stalin_sort.h
@@ -0,0 +1,23 @@
+//
+// Created by Michał on 04.04.2026.
+//
+#pragma once
+#include <list>
+
+inline std::list<int> stalin_sort(std::list<int> list) {
+    auto prev = list.begin();
+    auto present = ++list.begin();
+    while (present != list.end()) {
+        if (*prev > *present) {
+            auto temp = present;
+
+            ++present;
+            list.erase(temp);
+        }
+        else {
+            ++present;
+            ++prev;
+        }
+    }
+    return list;
+}
