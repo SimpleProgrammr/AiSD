023ed2a5978e8dcdb0380ff15474021123594d02
Programmr <michal.pelon@proton.me>
2026-04-04 18:53:06 +0200

    Minor bux fix

--- Sorting-Project/bubble_sort.h
+++ Sorting-Project/bubble_sort.h
@@ -9,7 +9,7 @@ inline int * bubble_sort(int * data, unsigned int length) {
 
     do {
         sorted = true;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length-1; i++) {
             if (data[i] > data[i + 1]) {
                 sorted = false;
                 std::swap(data[i], data[i + 1]);
